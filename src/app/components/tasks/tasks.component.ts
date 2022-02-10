8cd8f3c02b12d022a1f8cbe2827040008cdbdfe0
coper <coperni@gmail.com>
2022-02-10 19:06:04 -0300

    Funciones de eliminar y sumar

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { takeLast } from 'rxjs';
 import { TaskService } from '../../service/task.service';
 import {Task} from '../../Task'
 
@@ -19,5 +20,26 @@ export class TasksComponent implements OnInit {
     // Como una promesa, like promise
     this.taskService.getTasks().subscribe( tasks => this.tasks = tasks);
   }
+
+  deleteTask(task:Task){
+    this.taskService.deleteTask(task)
+    .subscribe(() => (
+      this.tasks = this.tasks.filter( (t) => t.id !== task.id )
+      
+    ))
+  }
+
+  toggleReminder(task:Task){
+    task.reminder = !task.reminder
+    this.taskService.updateTaskReminder(task).subscribe();
+  }
+
+  addTask(task:Task){
+    this.taskService.addTask(task).subscribe(task =>
+      this.tasks.push(task)    
+    )
+  }
+
+
 
 }
--- src/app/service/task.service.ts
+++ src/app/service/task.service.ts
@@ -1,8 +1,15 @@
 import { Injectable } from '@angular/core';
-import {HttpClient, HttpHandler} from '@angular/common/http';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
 import { Observable, of } from 'rxjs';
 import {Task} from '../Task';
 import {TASKS} from '../mock-task';
+
+
+const httpOptions = {
+  headers: new HttpHeaders({
+    'Content-Tupe':'application/json'
+  })
+}
 
 @Injectable({
   providedIn: 'root'
@@ -17,6 +24,21 @@ export class TaskService {
 
   getTasks(): Observable<Task[]>{
     return this.http.get<Task[]>(this.apliUrl)
+  }
+
+  deleteTask(task:Task): Observable<Task>{
+    const url = `${this.apliUrl}/${task.id}`
+    return this.http.delete<Task>(url)
+
+  }
+
+  updateTaskReminder(task: Task): Observable<Task>{
+    const url = `${this.apliUrl}/${task.id}`
+    return this.http.put<Task>(url, task, httpOptions)
+  }
+
+  addTask(task:Task): Observable<Task>{
+    return this.http.post<Task>(this.apliUrl,task,httpOptions)
   }
 
 }
